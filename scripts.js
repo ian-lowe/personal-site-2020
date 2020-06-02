177475d6821d616ba81659c6119bdb72bea3b93a
ian-lowe <ilowe@mac.com>
2020-06-02 16:00:04 -0700

    night/day mode

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 const moon = document.querySelector(".moon-container");
 const body = document.querySelector("body");
 
+document.documentElement.setAttribute("data-theme", "light");
+
 moon.addEventListener("click", function () {
-  body.classList.toggle("dark-mode");
+  let attribute = document.documentElement.getAttribute("data-theme");
+  console.log(attribute);
+  if (attribute === "light") {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
 });
