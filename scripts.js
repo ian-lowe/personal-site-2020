4c59cbfc1705c3d782940eb7e600d7c854cfaf1f
ian-lowe <ilowe@mac.com>
2020-06-02 16:17:12 -0700

    final submission

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
-const moon = document.querySelector(".moon-container");
+const moonContainer = document.querySelector(".moon-container");
+const moon = document.querySelector(".moon-container div");
 const body = document.querySelector("body");
 
 document.documentElement.setAttribute("data-theme", "light");
 
-moon.addEventListener("click", function () {
+moonContainer.addEventListener("click", function () {
   let attribute = document.documentElement.getAttribute("data-theme");
   console.log(attribute);
   if (attribute === "light") {
     document.documentElement.setAttribute("data-theme", "dark");
+    moon.id = "";
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    moon.id = "sun";
   }
 });
