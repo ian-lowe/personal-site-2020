76cb3664a23c04aaec3c28db148f31e4647eb8c3
ian-lowe <ilowe@mac.com>
2020-07-18 16:27:52 -0700

    updates

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
 const moonContainer = document.querySelector(".moon-container");
 const moon = document.querySelector(".moon-container div");
 const body = document.querySelector("body");
+const mode = document.querySelector("#mode");
 
 document.documentElement.setAttribute("data-theme", "light");
 
-moonContainer.addEventListener("click", function () {
+mode.addEventListener("click", function () {
   let attribute = document.documentElement.getAttribute("data-theme");
-  console.log(attribute);
   if (attribute === "light") {
     document.documentElement.setAttribute("data-theme", "dark");
-    moon.id = "";
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    moon.id = "sun";
   }
 });
+
+// moonContainer.addEventListener("click", function () {
+//   let attribute = document.documentElement.getAttribute("data-theme");
+//   if (attribute === "light") {
+//     document.documentElement.setAttribute("data-theme", "dark");
+//     moon.id = "";
+//   } else {
+//     document.documentElement.setAttribute("data-theme", "light");
+//     moon.id = "sun";
+//   }
+// });
