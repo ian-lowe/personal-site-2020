17075fb9098ebac1ee2450a71647183a9b8bdac7
ian-lowe <ilowe@mac.com>
2020-07-19 16:24:10 -0700

    added annimation

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-const moonContainer = document.querySelector(".moon-container");
-const moon = document.querySelector(".moon-container div");
 const body = document.querySelector("body");
 const mode = document.querySelector("#mode");
+const bio = document.querySelector("#bio");
+
+let speed = 100;
+typeEffect(bio, speed);
 
 document.documentElement.setAttribute("data-theme", "light");
 
@@ -14,13 +16,20 @@ mode.addEventListener("click", function () {
   }
 });
 
-// moonContainer.addEventListener("click", function () {
-//   let attribute = document.documentElement.getAttribute("data-theme");
-//   if (attribute === "light") {
-//     document.documentElement.setAttribute("data-theme", "dark");
-//     moon.id = "";
-//   } else {
-//     document.documentElement.setAttribute("data-theme", "light");
-//     moon.id = "sun";
-//   }
-// });
+function typeEffect(element, speed) {
+  var text = element.innerHTML;
+  element.innerHTML = "";
+
+  var i = 0;
+  var timer = setInterval(function () {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+window
+  .getComputedStyle(document.querySelector(".bio"), ":after")
+  .setProperty("display", "none");
